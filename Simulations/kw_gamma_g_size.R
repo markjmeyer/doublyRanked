0afc1a233d32849c71d6d9ee84363bad0aeee037
Mark J Meyer <35934764+markjmeyer@users.noreply.github.com>
2023-07-27 10:08:26 -0400

    Update kw_gamma_g_size.R

--- Simulations/kw_gamma_g_size.R
+++ Simulations/kw_gamma_g_size.R
@@ -3,7 +3,7 @@ library(refund)
 library(numbers)
 library(xtable)
 library(mvnfast)
-source('~/Dropbox/Research/Drafts/Wilcoxon FDA/wilcox.fda.R')
+source('wilcox.fda.R')
 
 #### specifications ####
 n1    <- n2   <-  n3 <- 10
@@ -13,7 +13,6 @@ sc    <- 1
 B     <- 1000
 sig   <- 0.05 # 0.5
 rho   <- c(0.25, 0.5, 0.75)
-# n1 <- 5; sn <- 1.25; S <- 160; sc <- 0.005
 dots  <- 1000
 up    <- 25000
 
@@ -44,9 +43,6 @@ for(i in 1:length(rho)){
       X2    <- rmvn(n = n2, mu = mu2, sigma = CovStr)
       X3    <- rmvn(n = n3, mu = mu3, sigma = CovStr)
       X     <- rbind(X1, X2, X3)
-      # med   <- which(fMBD2(t(X)) == max(fMBD2(t(X))))
-      # Xs    <- X - matrix(rep(X[med[1],], N), nrow = N, byrow = TRUE)
-      # Xs2   <- Xs^2
       fX    <- fpca.face(X)
       Y     <- fX$Yhat
       G     <- c(rep(0, n1), rep(1, n2), rep(2, n3))
@@ -74,10 +70,3 @@ for(i in 1:length(rho)){
 setwd('~/Dropbox/Research/Drafts/Wilcoxon FDA/Simulations/Gaussian/gamma/KW/Size')
 filename <- paste('kw_pnorm_g_size_', n1, '_fpca.RData', sep = '')
 save.image(file = filename)
-
-tabM  <- apply(pvals < 0.05, c(1,2), mean)
-
-# [c(1, 2, 4, 5, 3, 6)]
-tab <- matrix(apply(apply(pvals < 0.05, c(1,2), mean), 3, mean), nrow = 1)
-xtable(tab, digits = 4)
-
